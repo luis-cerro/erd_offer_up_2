70d8435ace56b81f7882a4907088e07b1c192121
anonymous anonymous <luis.cerro@kellogg.northwestern.edu>
2022-02-21 13:34:27 +0000

    Generate graphiti resources for items

---
+++ app/resources/item_resource.rb
@@ -0,0 +1,17 @@
+class ItemResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :item_description, :string
+  attribute :item_status_id, :integer
+  attribute :item_title, :string
+  attribute :item_category_id, :integer
+  attribute :item_location, :string
+  attribute :owner_id, :integer
+  attribute :price, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
